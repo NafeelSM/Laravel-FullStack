8185bb713a1c7a6bb2b4ead5a7a8f9251a39acb1
Nafeel SM <nafeelsm2018@gmail.com>
2024-10-10 22:51:48 +0530

    alignmentsCorrection

--- resources/js/Pages/Project/index.jsx
+++ resources/js/Pages/Project/index.jsx
@@ -11,68 +11,60 @@ export default function Index({ auth, projects }) {
                 </h2>
             }
         >
-        <Head title = "Projects"></Head>
-        <div className="py-12">
-            <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                    <div className="p-6 text-gray-900 dark:text-gray-100">
-
-                       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark: text-gray-400">
-                        <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark: bg-gray-700 dark: text-gray-400
-                        border-b-2 border-gray-500">
-                            <tr className="text-nowrap">
-                                <th className="px-3 py-3">ID</th>
-                                <th className="px-3 py-3">Image</th>
-                                <th className="px-3 py-3">Name</th>
-                                <th className="px-3 py-3">Status</th>
-                                <th className="px-3 py-3">Created Date</th>
-                                <th className="px-3 py-3">Due Date</th>
-                                <th className="px-3 py-3">Created By</th>
-                                <th className="px-3 py-3">Actions</th>
-
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {projects.data.map((project) => (
-                                <tr className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                                    <td className="px-3 py-2">{project.id}</td>
-                                    <td className="px-3 py-2">
-                                        <img src={project.image_path} style={{width:60}}/>
-                                    </td>
-                                    <td className="px-3 py-2">{project.name}</td>
-                                    <td className="px-3 py-2">{project.status}</td>
-                                    <td className="px-3 py-2">{project.created_at}</td>
-                                    <td className="px-3 py-2">{project.due_date}</td>
-                                    <td className="px-3 py-2">{project.createdBy.name}</td>
-                                    <td className="px-3 py-2">
-                                        <Link
-                                        href={route("project.edit", project.id)}
-                                        className="font-medium text-blue-600
-                                        dark:text-blue-500 hover:underline mx-1"
-                                        >
-                                         Edit
-                                        </Link>
-                                        <Link
-                                        href={route("project.destroy", project.id)}
-                                        className="font-medium text-red-600
-                                        dark:text-red-500 hover:underline mx-1"
-                                        >
-                                         Delete
-                                        </Link>
-
-                                    </td>
-
-                                </tr>
-                            ))}
-                        </tbody>
-                       </table>
-
+            <Head title="Projects" />
+            <div className="py-12">
+                <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                    <div className="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                        <div className="p-6 text-gray-900 dark:text-gray-100">
+                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400 border-b-2 border-gray-500">
+                                    <tr className="text-nowrap">
+                                        <th className="px-3 py-3">ID</th>
+                                        <th className="px-3 py-3">Image</th>
+                                        <th className="px-3 py-3">Name</th>
+                                        <th className="px-3 py-3">Status</th>
+                                        <th className="px-3 py-3">Created Date</th>
+                                        <th className="px-3 py-3">Due Date</th>
+                                        <th className="px-3 py-3">Created By</th>
+                                        <th className="px-3 py-3">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {projects.data.map((project) => (
+                                        <tr key={project.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                            <td className="px-3 py-2">{project.id}</td>
+                                            <td className="px-3 py-2">
+                                                <img src={project.image_path} alt={project.name} style={{ width: 60 }} />
+                                            </td>
+                                            <td className="px-3 py-2">{project.name}</td>
+                                            <td className="px-3 py-2">{project.status}</td>
+                                            <td className="px-3 py-2">{project.created_at}</td>
+                                            <td className="px-3 py-2">{project.due_date}</td>
+                                            <td className="px-3 py-2">{project.createdBy.name}</td>
+                                            <td className="px-3 py-2">
+                                                <Link
+                                                    href={route("project.edit", project.id)}
+                                                    className="font-medium text-blue-600 dark:text-blue-500 hover:underline mx-1"
+                                                >
+                                                    Edit
+                                                </Link>
+                                                <Link
+                                                    href={route("project.destroy", project.id)}
+                                                    method="delete"
+                                                    as="button"
+                                                    className="font-medium text-red-600 dark:text-red-500 hover:underline mx-1"
+                                                >
+                                                    Delete
+                                                </Link>
+                                            </td>
+                                        </tr>
+                                    ))}
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-
-
         </AuthenticatedLayout>
-    )
+    );
 }
