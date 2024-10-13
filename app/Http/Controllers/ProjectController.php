79336b5eca97977eadfa7760656117e0add9c4fd
Nafeel SM <nafeelsm2018@gmail.com>
2024-10-13 21:08:54 +0530

    create-project

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -7,6 +7,7 @@ use App\Http\Resources\ProjectResource;
 use App\Http\Requests\StoreprojectRequest;
 use App\Http\Requests\UpdateprojectRequest;
 use App\Http\Resources\TaskResource;
+use Illuminate\Support\Facades\Auth;
 
 class ProjectController extends Controller
 {
@@ -52,7 +53,12 @@ class ProjectController extends Controller
      */
     public function store(StoreprojectRequest $request)
     {
-        //
+        $data = $request->validated();
+        $data ['created_by'] = Auth::id();
+        $data ['updated_by'] = Auth::id();
+        project::create($data);
+
+        return to_route('project.index');
     }
 
     /**
